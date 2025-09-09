3db2bb068136e85abb64270f4178929bca1abfc5
KW <klymentwong@gmail.com>
2025-09-09 15:11:51 +0800

    Fix purple navigation blocks with high-specificity CSS overrides
    - Created navigation-fix.css with html body selectors for maximum CSS specificity
    - Updated theme/index.js to import both style.css and navigation-fix.css
    - Resolved VitePress navigation component styling conflicts in production builds
    - Fixed purple background blocks appearing in GitHub Pages deployment

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,13 +1,7 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-
-// Import default theme styles
-import 'vitepress/dist/client/theme-default/styles/vars.css'
-import 'vitepress/dist/client/theme-default/styles/base.css'
-import 'vitepress/dist/client/theme-default/styles/utils.css'
-import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
-import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
+import './navigation-fix.css'
 
 /** @type {import('vitepress').Theme} */
 export default DefaultTheme
